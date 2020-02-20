a5a11d5d539b6916f89be21e23a03a88e98f6fbb
CDulouard <clement.dulouard@gmail.com>
2020-02-20 16:55:49 +0100

    Add a function to creat connection messages

--- DotNetServer/ConsoleApplication1/Program.cs
+++ DotNetServer/ConsoleApplication1/Program.cs
@@ -12,11 +12,13 @@ namespace ConsoleApplication1
         public static void Main(string[] args)
         {
             var s = new UdpSocket();
-            s.Start("192.168.50.85", 50001, "test", verbose:true);
+            s.Start("192.168.43.100", 50000, "test", verbose:true);
             // s.Start("127.0.0.1", 27000, "test", verbose:true);
             
-            s.SendTo("192.168.50.85", 50000, "retest");
-            Thread.Sleep(1000);
+            // s.SendTo("192.168.43.81", 50000, "retest");
+            // Thread.Sleep(1000);
+            // s.SendTo("192.168.43.81", 50000, "retest");
+            // Thread.Sleep(1000);
             // s.SendTo("128.0.0.1", 27000, "tst");
             // Thread.Sleep(1000);
             // s.SendTo("127.0.0.2", 27000, "tst");
@@ -24,7 +26,8 @@ namespace ConsoleApplication1
             // s.SendTo("127.0.0.1", 27000, "retest");
             // Thread.Sleep(1000);
             // s.SendTo("127.0.0.1", 27000, "retest");
-            var msgToSend = new Message(5, "");
+            var msgToSend = Message.CreateConnectionMessage("test", hashPass:true);
+            Console.WriteLine(msgToSend);
             // s.CreatConnection(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 27000));
             // Thread.Sleep(1000);
             // s.Send("text");
