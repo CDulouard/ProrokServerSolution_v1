f6a051b4e92f3c6b19e119ec642941fad742a436
CDulouard <clement.dulouard@gmail.com>
2020-02-20 16:07:38 +0100

    Raph's special code

--- DotNetServer/ConsoleApplication1/Program.cs
+++ DotNetServer/ConsoleApplication1/Program.cs
@@ -12,17 +12,19 @@ namespace ConsoleApplication1
         public static void Main(string[] args)
         {
             var s = new UdpSocket();
-            s.Start("127.0.0.1", 27000, "test", verbose:true);
+            s.Start("192.168.50.85", 50001, "test", verbose:true);
+            // s.Start("127.0.0.1", 27000, "test", verbose:true);
             
-            s.SendTo("127.0.0.1", 27000, "retest");
+            s.SendTo("192.168.50.85", 50000, "retest");
             Thread.Sleep(1000);
-            s.SendTo("128.0.0.1", 27000, "tst");
-            Thread.Sleep(1000);
-            s.SendTo("127.0.0.2", 27000, "tst");
-            Thread.Sleep(1000);
-            s.SendTo("127.0.0.1", 27000, "retest");
-            Thread.Sleep(1000);
-            s.SendTo("127.0.0.1", 27000, "retest");
+            // s.SendTo("128.0.0.1", 27000, "tst");
+            // Thread.Sleep(1000);
+            // s.SendTo("127.0.0.2", 27000, "tst");
+            // Thread.Sleep(1000);
+            // s.SendTo("127.0.0.1", 27000, "retest");
+            // Thread.Sleep(1000);
+            // s.SendTo("127.0.0.1", 27000, "retest");
+            var msgToSend = new Message(5, "");
             // s.CreatConnection(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 27000));
             // Thread.Sleep(1000);
             // s.Send("text");
--- PythonClient/main.py
+++ PythonClient/main.py
@@ -4,11 +4,27 @@ import time
 
 if __name__ == "__main__":
     server = UdpSocket()
-    server.start_socket("127.0.0.1", 50000, "test")
-    for i in range(5):
-        time.sleep(1)
-        server.send_to(('127.0.0.1', 27000), str(Message(1, Message.creat_connection_message(server.hash_password, verbose=1))))
-        print("Attente")
-    server.send_to(('127.0.0.1', 27000), str(Message(2, "test")))
+    server.start_socket("192.168.50.85", 50000, "test")
+    # server.start_socket("127.0.0.1", 50000, "test")
+    # for i in range(5):
+    #     time.sleep(1)
+    #     server.send_to(('127.0.0.1', 27000), str(Message(1, Message.creat_connection_message(server.hash_password, verbose=1))))
+    #     print("Attente")
     # time.sleep(1)
+    for i in range(5):
+        print(str(Message(1, Message.spe_creat_connection_message(server.hash_password, verbose=1))))
+        server.send_to(('192.168.50.1', 50056),
+                       str(Message(1, Message.spe_creat_connection_message(server.hash_password, verbose=1))))
+        time.sleep(0.1)
+
+    time.sleep(1)
+    server.send_to(('192.168.50.1', 50056), str(Message(5, '{"targets": {"rAnkleRX": {"position": 0, "torque": 0}, "lAnkleRX": {"position": 0, "torque": 0}, "rAnkleRZ": {"position": 0, "torque": 0}, "lAnkleRZ": {"position": 0, "torque": 0}, "rShoulderRY": {"position": 0, "torque": 0}, "lShoulderBaseRY": {"position": 0, "torque": 0}, "rShoulderBaseRY": {"position": 0, "torque": 0}, "lShoulderRY": {"position": 0, "torque": 0}, "rShoulderRZ": {"position": 0, "torque": 0}, "lShoulderRZ": {"position": 0, "torque": 0}, "rKneeRX": {"position": 0, "torque": 0}, "lKneeRX": {"position": 0, "torque": 0}, "rHipRX": {"position": 0, "torque": 0}, "lHipRX": {"position": 0, "torque": 0}, "rHipRY": {"position": 0, "torque": 0}, "lHipRY": {"position": 0, "torque": 0}, "rHipRZ": {"position": 0, "torque": 0}, "lHipRZ": {"position": 0, "torque": 0}, "headRX": {"position": 0, "torque": 0}, "rElbowRX": {"position": 0, "torque": 0}, "lElbowRX": {"position": 0, "torque": 0}, "torsoRY": {"position": 0, "torque": 0}}}')))
+    time.sleep(1)
+    server.send_to(('192.168.50.1', 50056), str(Message(5,
+                                                        '{"targets": {"rAnkleRX": {"position": 0, "torque": 0}, "lAnkleRX": {"position": 0, "torque": 0}, "rAnkleRZ": {"position": 0, "torque": 0}, "lAnkleRZ": {"position": 0, "torque": 0}, "rShoulderRY": {"position": 0, "torque": 0}, "lShoulderBaseRY": {"position": 0, "torque": 0}, "rShoulderBaseRY": {"position": 0, "torque": 0}, "lShoulderRY": {"position": 0, "torque": 0}, "rShoulderRZ": {"position": 0, "torque": 0}, "lShoulderRZ": {"position": 0, "torque": 0}, "rKneeRX": {"position": 0, "torque": 0}, "lKneeRX": {"position": 0, "torque": 0}, "rHipRX": {"position": 0, "torque": 0}, "lHipRX": {"position": 0, "torque": 0}, "rHipRY": {"position": 0, "torque": 0}, "lHipRY": {"position": 0, "torque": 0}, "rHipRZ": {"position": 0, "torque": 0}, "lHipRZ": {"position": 0, "torque": 0}, "headRX": {"position": 0, "torque": 0}, "rElbowRX": {"position": 0, "torque": 0}, "lElbowRX": {"position": 0, "torque": 0}, "torsoRY": {"position": 0, "torque": 0}}}')))
+    time.sleep(1)
+    server.send_to(('192.168.50.1', 50056), str(Message(5,
+                                                        '{"targets": {"rAnkleRX": {"position": 0, "torque": 0}, "lAnkleRX": {"position": 0, "torque": 0}, "rAnkleRZ": {"position": 0, "torque": 0}, "lAnkleRZ": {"position": 0, "torque": 0}, "rShoulderRY": {"position": 0, "torque": 0}, "lShoulderBaseRY": {"position": 0, "torque": 0}, "rShoulderBaseRY": {"position": 0, "torque": 0}, "lShoulderRY": {"position": 0, "torque": 0}, "rShoulderRZ": {"position": 0, "torque": 0}, "lShoulderRZ": {"position": 0, "torque": 0}, "rKneeRX": {"position": 0, "torque": 0}, "lKneeRX": {"position": 0, "torque": 0}, "rHipRX": {"position": 0, "torque": 0}, "lHipRX": {"position": 0, "torque": 0}, "rHipRY": {"position": 0, "torque": 0}, "lHipRY": {"position": 0, "torque": 0}, "rHipRZ": {"position": 0, "torque": 0}, "lHipRZ": {"position": 0, "torque": 0}, "headRX": {"position": 0, "torque": 0}, "rElbowRX": {"position": 0, "torque": 0}, "lElbowRX": {"position": 0, "torque": 0}, "torsoRY": {"position": 0, "torque": 0}}}')))
+    time.sleep(10)
+
     server.stop_socket()
